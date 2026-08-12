144af7b69a48779b0ba279161ca0fe3fc82b6e5d
voyaqur <voyaqur@gmail.com>
2026-08-12 08:12:22 +0700

    removed some fancy stuff

--- .config/hypr/conf/rules.lua
+++ .config/hypr/conf/rules.lua
@@ -67,20 +67,20 @@ function config.setup()
     -- "Smart gaps" / "No gaps when only"
     -- uncomment all if you wish to use that.
 
-    -- hl.workspace_rule({ workspace = "w[tv1]", gaps_out = 0, gaps_in = 0 })
-    -- hl.workspace_rule({ workspace = "f[1]", gaps_out = 0, gaps_in = 0 })
-    -- hl.window_rule({
-    --     name        = "no-gaps-wtv1",
-    --     match       = { float = false, workspace = "w[tv1]" },
-    --     border_size = 0,
-    --     rounding    = 0,
-    -- })
-    -- hl.window_rule({
-    --     name        = "no-gaps-f1",
-    --     match       = { float = false, workspace = "f[1]" },
-    --     border_size = 0,
-    --     rounding    = 0,
-    -- })
+    hl.workspace_rule({ workspace = "w[tv1]", gaps_out = 0, gaps_in = 0 })
+    hl.workspace_rule({ workspace = "f[1]", gaps_out = 0, gaps_in = 0 })
+    hl.window_rule({
+        name        = "no-gaps-wtv1",
+        match       = { float = false, workspace = "w[tv1]" },
+        border_size = 0,
+        rounding    = 0,
+    })
+    hl.window_rule({
+        name        = "no-gaps-f1",
+        match       = { float = false, workspace = "f[1]" },
+        border_size = 0,
+        rounding    = 0,
+    })
 end
 
 return config
